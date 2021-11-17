b5f22c6942a3446a17d79156ca2f7f7a6e50c555
harrybradshaw <harrylewisbradshaw@googlemail.com>
2021-11-17 10:58:27 +0000

    Basic FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,28 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1; i < 101; i++)
+            {
+                string s = "";
+                if (i % 3 == 0)
+                {
+                    s += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    s += "Buzz";
+                }
+
+                if (s.Length != 0)
+                {
+                    Console.WriteLine(s);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
